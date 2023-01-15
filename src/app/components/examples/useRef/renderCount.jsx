b0ddb118eb10619986dbb4bea3489c0a97687534
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-15 14:02:16 +0100

    useRef => count amount of render component

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
-    const [renderCount, setRenderCount] = useState();
+    const renderCount = useRef(0);
+    const [testState, setTestState] = useState(false);
+    const toggleTest = () => {
+        setTestState(!testState);
+    };
     useEffect(() => {
-        setRenderCount(prevState => prevState + 1);
+        renderCount.current++;
     });
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
-            <p>renderCount:{renderCount}</p>
+            <p>render count:{renderCount.current}</p>
+            <button className={"btn btn-primary"} onClick={toggleTest}>test button</button>
         </CardWrapper>
     );
 };
