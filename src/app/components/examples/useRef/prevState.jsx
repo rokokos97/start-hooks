96bcfac321cf834101f65de4de77eda0a68e8021
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-15 14:28:57 +0100

    useRef => prevState.jsx => show prev state of component

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const PrevStateExample = () => {
+    const prevState = useRef("");
+    const [otherState, setOtherState] = useState("false");
+    const handleChangeState = () => {
+      setOtherState((prevState) => prevState === "false" ? "true" : "false");
+    };
+    useEffect(() => {
+        prevState.current = otherState;
+    }, [otherState]);
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <p>prev state: {prevState.current}</p>
+            <p>other state: {otherState}</p>
+            <button className={"btn btn-primary"} onClick={handleChangeState}>prev state</button>
         </CardWrapper>
     );
 };
