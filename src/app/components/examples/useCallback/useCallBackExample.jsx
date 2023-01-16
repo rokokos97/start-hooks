039ed90e5ed286123ef170108357698c05e5a0a7
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-16 18:43:51 +0100

    HOC => create HOC withLogin.jsx

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,22 +1,34 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
     const [data, setData] = useState({});
     const withOutCallBack = useRef(0);
+    const withCallBack = useRef(0);
     const handleChange = ({ target }) => {
         setData((prevState) => ({ ...prevState, [target.name]: target.value }));
     };
     const validateWithOutCallBack = (data) => {
         console.log(data);
     };
-    useEffect(() => { validateWithOutCallBack(data); }, [data]);
+    const validateWithCallBack = useCallback(
+        (data) => {
+            console.log(data);
+        },
+        []
+    );
+    useEffect(() => {
+        validateWithOutCallBack(data);
+        validateWithCallBack(data);
+        }, [data]);
     useEffect(() => { withOutCallBack.current++; }, [validateWithOutCallBack]);
+    useEffect(() => { withCallBack.current++; }, [validateWithCallBack]);
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
             <p>Render withOutCallBack: {withOutCallBack.current} </p>
+            <p>Render withCallBack: {withCallBack.current} </p>
             <label htmlFor="email" className="form-label">Email</label>
             <input
                 type="email"
