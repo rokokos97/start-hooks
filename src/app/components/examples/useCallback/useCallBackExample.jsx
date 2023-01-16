e92f0bc0528d36cbe6866eae639c07c2a0b8ad16
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-16 12:28:44 +0100

    useCallBack => create new state.

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
+    const [data, setData] = useState({});
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <label htmlFor="email" className="form-label">Email</label>
+            <input
+                type="email"
+                id="email"
+                className="form-control"
+                onChange={handleChange}
+                name={"email"}
+                value={data.email || ""}
+            />
         </CardWrapper>
     );
 };
