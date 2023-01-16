b27e3fb7a8f8608c25b8d35f68caa91c89703a70
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-16 12:38:56 +0100

    useCallBack => add useRef for calculate numbers of render

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
 const UseCallBackExample = () => {
     const [data, setData] = useState({});
+    const withOutCallBack = useRef(0);
     const handleChange = ({ target }) => {
         setData((prevState) => ({ ...prevState, [target.name]: target.value }));
     };
+    const validateWithOutCallBack = (data) => {
+        console.log(data);
+    };
+    useEffect(() => { validateWithOutCallBack(data); }, [data]);
+    useEffect(() => { withOutCallBack.current++; }, [validateWithOutCallBack]);
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <p>Render withOutCallBack: {withOutCallBack.current} </p>
             <label htmlFor="email" className="form-label">Email</label>
             <input
                 type="email"
