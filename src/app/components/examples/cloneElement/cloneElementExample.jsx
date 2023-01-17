396f7045b014038e4cf86bc90bff47676db489a5
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-17 12:57:36 +0100

    HOC => homework complete

--- src/app/components/examples/cloneElement/cloneElementExample.jsx
+++ src/app/components/examples/cloneElement/cloneElementExample.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import CardWrapper from "../../common/Card";
 
 import SmallTitle from "../../common/typografy/smallTitle";
+import TextField from "../../common/form/textField";
+import Divider from "../../common/divider";
 const CloneElementExample = () => {
+    const field = <TextField name={"email"} label={"Email"}/>;
+    const handleChange = (target) => {
+        console.log("change:", target);
+    };
     return (
         <CardWrapper>
             <SmallTitle>Пример</SmallTitle>
+            <Divider/>
+            { field }
+            {React.cloneElement(field, { onChange: handleChange, label: "Clone element" })}
         </CardWrapper>
     );
 };
--- src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,5 +1,26 @@
+import React, { useCallback, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+const LogOutButton = ({ onLogOut }) => {
+    useEffect(() => {
+        console.log("render button");
+    });
+ return <button className="btn btn-primary" onClick={onLogOut}>Log out</button>;
+};
+LogOutButton.propTypes = {
+    onLogOut: PropTypes.func
+};
+
+const MemoizedLogOutButton = React.memo(LogOutButton);
 const MemoWithUseCallbackExample = (props) => {
-    return null;
+    const [state, setState] = useState(false);
+    const handleLogOut = useCallback(() => {
+        localStorage.removeItem("auth");
+    }, [props]);
+    return <>
+        <button className="btn btn-primary m-1" onClick={() => { setState(!state); }}>Initiate rerender</button>
+        <MemoizedLogOutButton onLogOut={handleLogOut}/>
+    </>;
 };
 
 export default MemoWithUseCallbackExample;
